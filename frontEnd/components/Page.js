3a6b8c162aa9a64481472cbc5ecd476f4c84e3a8
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-05-14 14:45:20 +0200

    Feat: add Header component and proptypes

--- frontEnd/components/Page.js
+++ frontEnd/components/Page.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import Header from './Header';
+import PropTypes from 'prop-types';
 
 export default class Page extends Component {
     render() {
         return (
             <div>
-                <p>I am the page component!</p>
+                <Header storeTitle={this.props.storeTitle} />
                 {this.props.children}
             </div>
         );
     }
 }
+
+Page.PropTypes = {
+    storeTitle: PropTypes.string
+};
