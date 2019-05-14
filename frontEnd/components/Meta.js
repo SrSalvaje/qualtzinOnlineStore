ced0b72ddcba1669229708e043e1dd7a283bb536
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-05-14 15:14:06 +0200

    Feat: create Meta component

---
+++ frontEnd/components/Meta.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Meta = props => (
+    <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <link rel="shortcut icon" href="/static/favicon.png" />
+        <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+        <title>{props.storeTitle}</title>
+    </Head>
+);
+
+Meta.PropTypes = {
+    storeTitle: PropTypes.string
+};
+
+export default Meta;
