596510509b33292685052ec3005de32d7598f777
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-05-14 15:14:53 +0200

    Feat: add proptypes and  Meta and Header Components

--- frontEnd/components/Page.js
+++ frontEnd/components/Page.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Meta from './Meta';
 import PropTypes from 'prop-types';
 
 export default class Page extends Component {
     render() {
         return (
             <div>
+                <Meta storeTitle={this.props.storeTitle} />
                 <Header storeTitle={this.props.storeTitle} />
                 {this.props.children}
             </div>
