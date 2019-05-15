7a4a30e6df2bcd459e5f1f4fbcc5de07cb8dd5d8
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-05-15 13:57:02 +0200

    Feat:  Add static method from custom <App> docs

--- frontEnd/pages/_app.js
+++ frontEnd/pages/_app.js
@@ -2,16 +2,29 @@ import App, { Container } from 'next/app';
 import Page from '../components/Page/Page';
 
 class MyApp extends App {
+    // From the docs, needed to make a custom <App>
+    static async getInitialProps({ Component, ctx }) {
+        let pageProps = {};
+
+        if (Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx);
+        }
+
+        return { pageProps };
+    }
+
+    // * Here is State
     state = {
         storeTitle: 'My Store Name'
     };
+
     render() {
-        const { Component } = this.props;
+        const { Component, pageProps } = this.props;
 
         return (
             <Container>
                 <Page storeTitle={this.state.storeTitle}>
-                    <Component />
+                    <Component {...pageProps} />
                 </Page>
             </Container>
         );
