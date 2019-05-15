e5383a6a16b31bf24301afaa3bc959e6e2fac83f
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-05-15 15:02:30 +0200

    Feat: change favicon and fix proptypes
    The previous favicon is too big, chrome doesnt render it.
    
    component.proptypes shouldnt be capitalized.

--- frontEnd/components/Meta/Meta.js
+++ frontEnd/components/Meta/Meta.js
@@ -5,13 +5,13 @@ const Meta = props => (
     <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
-        <link rel="shortcut icon" href="/static/favicon.png" />
+        <link rel="shortcut icon" href="/static/debit-card.png" />
         <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
         <title>{props.storeTitle}</title>
     </Head>
 );
 
-Meta.PropTypes = {
+Meta.propTypes = {
     storeTitle: PropTypes.string
 };
 
