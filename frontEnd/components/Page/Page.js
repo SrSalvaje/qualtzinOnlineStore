500bebf268d98870fde042f850082341b6d53a81
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-05-16 11:55:11 +0200

    Feat: Add styled-components

--- frontEnd/components/Page/Page.js
+++ frontEnd/components/Page/Page.js
@@ -2,16 +2,24 @@ import React, { Component } from 'react';
 import Header from '../Header/Header';
 import Meta from '../Meta/Meta';
 import PropTypes from 'prop-types';
+//theme used by styled-components, it has any css values I might reuse
+import theme1 from '../../global Styles/theme';
+// ! Needed by styled-components to access the theme, its based on the React Context api
+import { ThemeProvider, injectGlobal } from 'styled-components';
+// my styled components
+import { StyledMainDiv, StyledInnerDiv } from './stylesPage';
 
 export default class Page extends Component {
     render() {
         return (
-            <div>
-                <Meta storeTitle={this.props.storeTitle} />
-                <Header storeTitle={this.props.storeTitle} />
-                <p>I am everywhere</p>
-                {this.props.children}
-            </div>
+            <ThemeProvider theme={theme1}>
+                <StyledMainDiv>
+                    <Meta storeTitle={this.props.storeTitle} />
+                    <Header storeTitle={this.props.storeTitle} />
+                    <p>I am everywhere</p>
+                    <StyledInnerDiv>{this.props.children}</StyledInnerDiv>
+                </StyledMainDiv>
+            </ThemeProvider>
         );
     }
 }
