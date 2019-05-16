91f3949500062518eb97392d5e74acf49af2d7ae
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-05-16 16:39:43 +0200

    feat: add global styles

--- frontEnd/components/Page/Page.js
+++ frontEnd/components/Page/Page.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
-import Header from '../Header/Header';
-import Meta from '../Meta/Meta';
 import PropTypes from 'prop-types';
-//theme used by styled-components, it has any css values I might reuse
-import theme1 from '../../global Styles/theme';
 // ! Needed by styled-components to access the theme, its based on the React Context api
-import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 // my styled components
 import { StyledMainDiv, StyledInnerDiv } from './stylesPage';
+import GlobalStyle from '../../global Styles/globalStyle';
+//theme used by styled-components, it has any css values I might reuse
+import theme1 from '../../global Styles/theme';
+// components
+import Meta from '../Meta/Meta';
+import Header from '../Header/Header';
 
 export default class Page extends Component {
     render() {
         return (
             <ThemeProvider theme={theme1}>
-                <StyledMainDiv>
-                    <Meta storeTitle={this.props.storeTitle} />
-                    <Header storeTitle={this.props.storeTitle} />
-                    <p>I am everywhere</p>
-                    <StyledInnerDiv>{this.props.children}</StyledInnerDiv>
-                </StyledMainDiv>
+                {/*need it in order to add GlobalStyle*/}
+                <React.Fragment>
+                    <StyledMainDiv>
+                        <Meta storeTitle={this.props.storeTitle} />
+                        <Header storeTitle={this.props.storeTitle} />
+                        <p>I am everywhere</p>
+                        <StyledInnerDiv>{this.props.children}</StyledInnerDiv>
+                    </StyledMainDiv>
+                    <GlobalStyle />
+                </React.Fragment>
             </ThemeProvider>
         );
     }
