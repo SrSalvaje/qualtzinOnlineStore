064a8fe5dcc086d487ef5164d0c3295f008dd26c
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-05-16 18:41:02 +0200

    Feat: add nprogress

--- frontEnd/components/Header/Header.js
+++ frontEnd/components/Header/Header.js
@@ -1,9 +1,21 @@
 import Link from 'next/link';
 import { ThemeProvider, createGlobalStyle } from 'styled-components';
 import PropTypes from 'prop-types';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import { Logo, StyledHeader } from './stylesHeader';
 import theme1 from '../../global Styles/theme';
 import Nav from '../Nav/Nav';
+
+Router.onRouteChangeStart = () => {
+    NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+    NProgress.done();
+};
+Router.onRouteChangeError = () => {
+    NProgress.done();
+};
 
 const Header = props => (
     <ThemeProvider theme={theme1}>
--- frontEnd/components/Meta/Meta.js
+++ frontEnd/components/Meta/Meta.js
@@ -6,6 +6,8 @@ const Meta = props => (
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <link rel="shortcut icon" href="/static/debit-card.png" />
+        <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+
         <title>{props.storeTitle}</title>
     </Head>
 );
