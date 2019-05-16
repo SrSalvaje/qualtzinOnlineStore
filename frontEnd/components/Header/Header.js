c30341a8fcc6b2cb1b178fc2fbd43fb7f45abe2b
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-05-16 13:54:13 +0200

    Feat: apply basic styles to header

--- frontEnd/components/Header/Header.js
+++ frontEnd/components/Header/Header.js
@@ -1,20 +1,27 @@
 import Nav from '../Nav/Nav';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
+import { Logo, StyledHeader } from './stylesHeader';
+import theme1 from '../../global Styles/theme';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 
 const Header = props => (
-    <div>
-        <div className="bar">
-            <Link href="/">
-                <a>{props.storeTitle}</a>
-            </Link>
-            <Nav />
-        </div>
-        <div className="sub-bar">
-            <p>Search</p>
-        </div>
-        <div>Cart</div>
-    </div>
+    <ThemeProvider theme={theme1}>
+        <StyledHeader>
+            <div className="bar">
+                <Logo>
+                    <Link href="/">
+                        <a>{props.storeTitle}</a>
+                    </Link>
+                </Logo>
+                <Nav />
+            </div>
+            <div className="search-bar">
+                <p>Search</p>
+            </div>
+            <div>Cart</div>
+        </StyledHeader>
+    </ThemeProvider>
 );
 
 Header.propTypes = {
